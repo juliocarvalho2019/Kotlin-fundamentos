519750156e6466e037677a9744db0ffa287bf0ee
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-04-29 10:28:08 -0300

    Feat: estrutura when.

---
+++ fundamentos/src/main/kotlin/fundamentosWhen/when.kt
@@ -0,0 +1,15 @@
+package fundamentosWhen
+
+fun main() {
+
+    val x = 15
+
+    when(x){
+        5 -> println("x == 5")
+        8 -> println("x == 8")
+        9 -> println("x == 9")
+        15 -> println("x == 15")
+    }
+
+}
+
