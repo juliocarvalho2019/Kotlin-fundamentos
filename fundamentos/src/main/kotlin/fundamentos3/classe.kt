e3b2f418d1c634e3de40ba55323734753655709b
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-04-29 10:03:17 -0300

    Feat: Funções.

--- fundamentos/src/main/kotlin/fundamentos3/classe.kt
+++ fundamentos/src/main/kotlin/fundamentos3/classe.kt
@@ -1,2 +1,17 @@
 package fundamentos3
 
+fun main(){
+    println(retornaName())
+
+    dizOi(retornaName(), 24)
+    dizOi(idade = 24, nome ="Gustavo")
+    dizOi("Daniel")
+}
+
+fun retornaName(): String {
+    return "Gustavo"
+}
+
+fun dizOi(nome: String, idade: Int = 20){
+    println("Oi ${nome}, parábens pelo seus ${idade} anos")
+}
